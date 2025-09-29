b73e2bf1077b76a4884d6d6217ad8e735ae39cf9
hyodongg <whgyehdjhd@naver.com>
2025-09-29 15:47:54 +0900

    Init:초기 세팅 후 작동 확인

--- Frontend/app/src/main/java/com/example/iace2_frontend/MainActivity.kt
+++ Frontend/app/src/main/java/com/example/iace2_frontend/MainActivity.kt
@@ -3,45 +3,50 @@ package com.example.iace2_frontend
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.iace2_frontend.ui.theme.IACE2_FrontendTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             IACE2_FrontendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    var text by remember { mutableStateOf("Hello Android!") }
+
+    // 화면 중앙에 텍스트와 버튼 배치
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text)
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = { text = "버튼 눌렀다!" }) {
+            Text("눌러봐")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyAppPreview() {
     IACE2_FrontendTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
